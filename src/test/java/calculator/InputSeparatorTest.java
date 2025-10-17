c5603938f1cc9fae029449a8d3bd97ec3e8f3c0e
oseguri <canaein@gmail.com>
2025-10-17 22:13:29 +0900

    test(inputseparatortest) : 숫자 분리를 위한 클래스 테스트 작성

---
+++ src/test/java/calculator/InputSeparatorTest.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputSeparatorTest {
+
+
+    @DisplayName("숫자 분리 테스트")
+    @Test
+    public void separatorTest() {
+        List<Integer> list = Arrays.asList(1,223,45,5);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(InputSeparator.getSeparatedNums("//!\\n1!223,45!5"), list),
+                () -> Assertions.assertTrue(InputSeparator.getSeparatedNums("").isEmpty())
+        );
+    }
+
+}
